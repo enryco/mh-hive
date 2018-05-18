ed6543f9366a141ab4c6f77c58e1ee65a622c209
Enrico Scherlies <e.scherlies@me.com>
2018-05-18 21:47:33 +0200

    .

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+  state = {}
+
+  render() {
+
+    return (
+      <div className="mhh-header">
+      </div>
+    )
+  }
+}
+
+export default Header
